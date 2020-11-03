da861ece94764b619ac0029854b894570cfa3028
colincassens <ccassens@purdue.edu>
2020-11-02 20:48:48 -0500

    Initial Functionality Complete
    1) Async Working
    2) Randomize Answers
    3) Changing Render Element Based on State of Site

--- trivia_app/src/index.js
+++ trivia_app/src/index.js
@@ -3,81 +3,118 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-//var element = React.createElement('h1', {className: 'greeting'}, 'Welcome to Trivia!')
-
 class Quiz extends Component {
   constructor(props) {
-    super();
+    super()
     this.state = {
-      name: "React"
-    };
-    this.onValueChange = this.onValueChange.bind(this);
-    this.formSubmit = this.formSubmit.bind(this);
-    this.Q = props.Q
+      points: 0,
+      id: 0,
+      in_progress: true,
+    }
+    this.onValueChange = this.onValueChange.bind(this)
+    this.formSubmit = this.formSubmit.bind(this)
+    this.questions = props.questions
   }
 
   onValueChange(event) {
     this.setState({
       selectedOption: event.target.value
-    });
+    })
   }
 
   formSubmit(event) {
     event.preventDefault();
     console.log(this.state.selectedOption)
+    // Check to see if correct answer is submitted
+    if(this.state.selectedOption === this.questions[this.state.id].correct_answer){
+      this.setState({
+        id: this.state.id + 1,
+        points: this.state.points + 10
+      })
+    }else{
+      this.setState({
+        id: this.state.id + 1
+      })
+    }
+    if(this.state.id + 1 === this.questions.length){
+      this.setState({
+        in_progress: false
+      })
+    }
   }
 
   render() {
+    var {in_progress} = this.state
     return (
-      <form onSubmit={this.formSubmit}>
-        <div>
-          <h1>
-            Trivia!
-          </h1>
-          <h3>
-            {this.Q}
-          </h3>
-        </div>
-        <div className="radio">
-          <label>
-            <input
-              type="radio"
-              value="Option1"
-              checked={this.state.selectedOption === "Option1"}
-              onChange={this.onValueChange}
-            />
-            Option1
-          </label>
-        </div>
-        <div className="radio">
-          <label>
-            <input
-              type="radio"
-              value="Option2"
-              checked={this.state.selectedOption === "Option2"}
-              onChange={this.onValueChange}
-            />
-            Option2
-          </label>
-        </div>
-        <div className="radio">
-          <label>
-            <input
-              type="radio"
-              value="Option3"
-              checked={this.state.selectedOption === "Option3"}
-              onChange={this.onValueChange}
-            />
-            Option3
-          </label>
-        </div>
-        <div>
-          Selected option is : {this.state.selectedOption}
-        </div>
-        <button className="btn btn-default" type="submit">
-          Submit
-        </button>
-      </form>
+      <div>
+        {in_progress ? (<form onSubmit={this.formSubmit}>
+            <div>
+              <h1>
+                Trivia!
+              </h1>
+              <h2>
+                Current Score: {this.state.points}
+              </h2>
+              <h2>
+                Question Number #{this.state.id + 1}
+              </h2>
+              <h3>
+                {this.questions[this.state.id].question}
+              </h3>
+            </div>
+            <div className="radio">
+              <label>
+                <input
+                  type="radio"
+                  value={this.questions[this.state.id].rand[0]}
+                  checked={this.state.selectedOption === this.questions[this.state.id].rand[0]}
+                  onChange={this.onValueChange}
+                />
+                {this.questions[this.state.id].rand[0]}
+              </label>
+            </div>
+            <div className="radio">
+              <label>
+                <input
+                  type="radio"
+                  value={this.questions[this.state.id].rand[1]}
+                  checked={this.state.selectedOption === this.questions[this.state.id].rand[1]}
+                  onChange={this.onValueChange}
+                />
+                {this.questions[this.state.id].rand[1]}
+              </label>
+            </div>
+            <div className="radio">
+              <label>
+                <input
+                  type="radio"
+                  value={this.questions[this.state.id].rand[2]}
+                  checked={this.state.selectedOption === this.questions[this.state.id].rand[2]}
+                  onChange={this.onValueChange}
+                />
+                {this.questions[this.state.id].rand[2]}
+              </label>
+            </div>
+            <div className="radio">
+              <label>
+                <input
+                  type="radio"
+                  value={this.questions[this.state.id].rand[3]}
+                  checked={this.state.selectedOption === this.questions[this.state.id].rand[3]}
+                  onChange={this.onValueChange}
+                />
+                {this.questions[this.state.id].rand[3]}
+              </label>
+            </div>
+            <div>
+              Selected option is : {this.state.selectedOption}
+            </div>
+            <button className="btn btn-default" type="submit">
+              Submit
+            </button>
+          </form> ): <h1> Quiz Complete! You Finished With {this.state.points} Points</h1>
+        }
+      </div>
     );
   }
 }
@@ -85,25 +122,50 @@ class Quiz extends Component {
 class Questions extends Component{
   constructor(){
     super()
-    this.questions = []
-    this.getQuestions = this.getQuestions.bind(this)
-    this.questions = this.getQuestions()
+    this.state = {
+      questions: [],
+      loading: true
+    }
+  }
+
+  componentDidMount(){
+    this.getQuestions().then(qs => {
+      console.log(qs.results)
+      var holder = qs.results
+      holder.map((x) => {
+        x['rand'] = this.randomize_order(x)
+      })
+      this.setState({
+        questions: holder,
+        loading: false
+      })
+    })
+  }
+
+  randomize_order(question){
+    const { incorrect_answers, correct_answer } = question
+    incorrect_answers.push(correct_answer)
+    var shuffled = incorrect_answers
+      .map((x) => ({sort: Math.random(), value: x}))
+      .sort((a,b) => a.sort - b.sort)
+      .map((x) => x.value)
+    return shuffled
   }
 
   async getQuestions(){
     return new Promise((resolve, reject) =>{
       var questions
       let request = new XMLHttpRequest()
-      request.open("GET", "https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple")
+      request.open("GET", "https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple")
       request.send()
       request.onload = () =>{
-        console.log(request)
+        // console.log(request)
         if(request.status === 200) {
           questions = JSON.parse(request.response)
-          console.log(questions)
+          // console.log(questions)
           resolve(questions)
         } else {
-          console.log('Error')
+          // console.log('Error')
           questions = 'ERR'
           reject(questions)
         }
@@ -112,22 +174,12 @@ class Questions extends Component{
   }
 
   render() {
-    return this.questions.then(function(qarray){
-      return(
-        <div>
-          {qarray.results.map(q =>{
-            const { question, correct_answer, incorrect_answers } = q
-            return(
-              <Quiz
-                Q = {question}
-                CA = {correct_answer}
-                IAs = {incorrect_answers}
-              />
-            )
-          })}
-        </div>
-      )
-    })
+    const {questions, loading} = this.state
+    return(
+      <div>
+        {loading ? <h2>Loading...</h2> : <Quiz questions = {questions}/>}
+      </div>      
+    )
   }
 }
 
