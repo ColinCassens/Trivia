b7c038d8aa84a4cb9681869f330c03a9052553ba
colincassens <ccassens@purdue.edu>
2020-11-04 15:22:48 -0500

    Loading Spinner Added
    + Small Changes and Comments

--- trivia_app/src/App.js
+++ trivia_app/src/App.js
@@ -1,18 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import Questions from './index.js';
-import { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import Questions from './index.js';
+// //import { Component } from 'react';
 
-class App extends Component{
-  render(){
-    return (
-      <div className="App">
-         <header className="App-header">
-          <Questions />
-         </header>
-       </div>
-    );
-  }
-}
+// // class App extends Component{
+// //   render(){
+// //     return (
+// //       <div className="App">
+// //          <header className="App-header">
+// //           <Questions />
+// //          </header>
+// //        </div>
+// //     );
+// //   }
+// // }
 
-export default App;
+// // export default App;
--- trivia_app/src/index.js
+++ trivia_app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import './App.css';
 import reportWebVitals from './reportWebVitals';
+import Loader from 'react-loader-spinner';
 
 class Quiz extends Component {
   constructor(props) {
@@ -10,7 +11,7 @@ class Quiz extends Component {
     this.state = {
       points: 0,
       id: 0,
-      in_progress: true,
+      in_progress: true
     }
     this.onValueChange = this.onValueChange.bind(this)
     this.formSubmit = this.formSubmit.bind(this)
@@ -26,18 +27,17 @@ class Quiz extends Component {
   formSubmit(event) {
     event.preventDefault();
     console.log(this.state.selectedOption)
+    this.setState({
+      id: this.state.id + 1,
+      selectedOption: ''
+    })
+
     // Check to see if correct answer is submitted
     if(this.state.selectedOption === this.questions[this.state.id].correct_answer){
       this.setState({
-        id: this.state.id + 1,
         points: this.state.points + 10
       })
-    }else{
-      this.setState({
-        id: this.state.id + 1
-      })
-    }
-    if(this.state.id + 1 === this.questions.length){
+    } if(this.state.id + 1 === this.questions.length){
       this.setState({
         in_progress: false
       })
@@ -120,6 +120,7 @@ class Quiz extends Component {
   }
 }
 
+//Class for getting the Questions from the API and calling Quiz
 class Questions extends Component{
   constructor(){
     super()
@@ -129,6 +130,10 @@ class Questions extends Component{
     }
   }
 
+  /* Wait for the API call to mount
+      Once mounted save questions
+      and change loading to false
+  */
   componentDidMount(){
     this.getQuestions().then(qs => {
       console.log(qs.results)
@@ -143,6 +148,7 @@ class Questions extends Component{
     })
   }
 
+  //Randomize Answer Order for Quiz
   randomize_order(question){
     const { incorrect_answers, correct_answer } = question
     incorrect_answers.push(correct_answer)
@@ -153,6 +159,7 @@ class Questions extends Component{
     return shuffled
   }
 
+  //Get Questions from API using Promise
   async getQuestions(){
     return new Promise((resolve, reject) =>{
       var questions
@@ -160,14 +167,11 @@ class Questions extends Component{
       request.open("GET", "https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple")
       request.send()
       request.onload = () =>{
-        // console.log(request)
         if(request.status === 200) {
           questions = JSON.parse(request.response)
-          // console.log(questions)
           resolve(questions)
         } else {
-          // console.log('Error')
-          questions = 'ERR'
+          questions = 'ERR IN API CALL'
           reject(questions)
         }
       }
@@ -178,10 +182,22 @@ class Questions extends Component{
     const {questions, loading} = this.state
     return(
       <div>
-        {loading ? <h2>Loading...</h2> : <Quiz questions = {questions}/>}
+        {loading ? LoadSpinner() : <Quiz questions = {questions}/>}
       </div>  
     )
   }
+}
+
+// Loading Spinner settings
+function LoadSpinner (){
+  return (
+    <Loader
+      type='TailSpin'
+      color='#61dafb'
+      height='100'
+      width='100'
+    />
+  )
 }
 
 class App extends Component{
